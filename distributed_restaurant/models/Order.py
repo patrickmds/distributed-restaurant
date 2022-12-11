ab66ad490ec051fbcee77bca9c8abef6a6aab40a
Patrick <patricksjb1@gmail.com>
2022-12-11 04:30:42 -0300

    Adds template classes to implement stuff in the future

---
+++ distributed_restaurant/models/Order.py
@@ -0,0 +1,5 @@
+
+class Order:
+    def __init__(self, item_id, quantity):
+        self.item_id = item_id
+        self.quantity = quantity
---
+++ distributed_restaurant/models/RestaurantItem.py
@@ -0,0 +1,5 @@
+
+class RestaurantItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
